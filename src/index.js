10fa154b6b7f04f6271fda3ebb49800f282e426f
Jeremiah Melfah <jeremiahmelfah@Jeremiahs-MacBook-Air.local>
2022-09-27 23:37:28 +0000

    complete todo list

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
-import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
-
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
-}
-
-document.body.appendChild(component());
+const list = [
+  {
+    description: 'Walk the dog',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Take the car to the mechanic',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'Do the laundry',
+    completed: false,
+    index: 3,
+  },
+  {
+    description: 'Buy groceries',
+    completed: false,
+    index: 4,
+  },
+];
+document.addEventListener('DOMContentLoaded', () => {
+  const listContainer = document.querySelector('.todolist');
+  list.forEach((list) => {
+    const listHtml = `<li class="list-item" id="${list.index}"> <div><i class="fa-regular fa-square-full"></i> <span class="task">${list.description}</span></div><i class="fa-solid fa-ellipsis-vertical"></i></li>`;
+    listContainer.insertAdjacentHTML('beforeend', listHtml);
+  });
+});
